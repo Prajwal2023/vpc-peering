3333c7caaaa8fdec8d8d9c51a37e7b8d7aefd4e9
prajwal_patil <prajwalp721>
2024-03-12 00:02:02 +0530

    add

--- sub.tf
+++ sub.tf
@@ -4,11 +4,11 @@
 resource "aws_subnet" "subnet1" {
   vpc_id            = aws_vpc.vpc1.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
+  availability_zone = "ap-south-1a"
 }
 
 resource "aws_subnet" "subnet2" {
   vpc_id            = aws_vpc.vpc2.id
   cidr_block        = "10.1.1.0/24"
-  availability_zone = "us-west-2b"
+  availability_zone = "ap-south-1b"
 }
