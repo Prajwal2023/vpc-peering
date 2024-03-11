8479e50c0d53e64915e0fbedc0fd9578f8b743fc
prajwal_patil <prajwalp721>
2024-03-12 00:11:11 +0530

    change

--- igw.tf
+++ igw.tf
@@ -5,8 +5,9 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.vpc1.id
 }
 
-# Attach internet gateway to VPC
-resource "aws_vpc_attachment" "attachment" {
-  vpc_id       = aws_vpc.vpc1.id
-  internet_gateway_id = aws_internet_gateway.gw.id
+# Associate internet gateway with VPC route table
+resource "aws_route" "route_to_internet" {
+  route_table_id            = aws_route_table.vpc1_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id                = aws_internet_gateway.gw.id
 }
